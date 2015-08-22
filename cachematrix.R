a2c33d258363bdb29819162f71041ea05d23b620
David Hagler <davidhagler@gmail.com>
2015-08-22 10:51:05 -0400

    Test upate
    Added comments to verify I can upload this.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will create and use a cache for the results of a matrix operation
+## Instead of comptuing the inverse of a matrix, it will allow the inverse to be cached and used
 
-## Write a short comment describing this function
+## This function will create a function with 4 funtions, set matrix, get matrix, set inverse, get inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
